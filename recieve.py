73fec3eff8d2eda9af560ffdf2b5f50ece28c797
nmd1 <rafty132@gmail.com>
2017-12-03 10:01:26 -0500

    updated bit stream
    changed the script that recieves raw bits from the arduino be composed of callable functions, to make it easy to code the GUI

--- recieve.py
+++ recieve.py
@@ -1,33 +1,43 @@
-from __future__ import print_function
+#from __future__ import print_function 	# I converted this to python 3 now
 import RPi.GPIO as GPIO
 import time
-import easygui
-
-pins = [0,5,13,19,26,16,21,20]
-pins = list(reversed(pins))
 
 GPIO.setmode(GPIO.BCM)
+pins = [0,5,13,19,26,16,21,20]
+pins = list(reversed(pins)) 		# The pins are reversed in the arduino, so we flip it back here.
+
 for pin in pins:
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def getpsi(val):
-	volts = (val/254.0) * 5;
-	outv = (volts/0.24) - 0.5;
-	psi = (outv * 10.1379);
+def convert(val):
+	volts = (val/254.0) * 5;	# rescale value to voltage (2^8 wires)
+	outv = (volts/0.24) - 0.5;	# raw math (credit to cho)
+	psi = (outv * 10.1379);		# more math
 	return psi
 
-while True:
-	rawbin = []	
+def getBits():
+	rawbin = []
 	for pin in pins:
-		rawbin.append(GPIO.input(pin))
-	print(rawbin,end='')
+                rawbin.append(GPIO.input(pin))
+	return rawbin
+
+def debugReturnPSI():	
+	rawbinary = getBits()		# get bits from arduino
 	binary = ""
-	for unit in rawbin:
+	for unit in rawbinary:		# convert array to string
     		binary += str(unit)
-	value = int(binary, 2)
-        print(getpsi(value))
+	value = int(binary, 2)		# convert bin string to int
+	psi = convert(value)		# do math conversion
+	return psi,rawbinary
 	
+	
+def returnPSI():
+	return debugReturnPSI()[0]	# wrap debug function for external use
+
+while True:
+	psi = debugReturnPSI()
+	print(psi[1],end=' ')
+	print(psi[0])
 	time.sleep(.01)
-	
 	
 
